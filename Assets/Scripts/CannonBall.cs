8591dbd5d7dd9187e6c197e0f43236ed75757207
Noah Bathras <nbathras@terpmail.umd.edu>
2020-11-13 22:56:03 -0500

    Refactored player controller and cannon ball movement to be physics based

---
+++ Assets/Scripts/CannonBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CannonBall : MonoBehaviour {
+    public static CannonBall CreateCannonBall(Vector3 inPosition, Vector3 inDirNormalized, Vector3 inCurrentVelocity) {
+        Transform pfCannonBall = Resources.Load<Transform>("pfCannonBall");
+        Transform cannonBallTransform = Instantiate(pfCannonBall, inPosition, Quaternion.identity);
+
+        CannonBall cannonBall = cannonBallTransform.GetComponent<CannonBall>();
+        cannonBall.rb.velocity = inDirNormalized * cannonBall.speed + inCurrentVelocity;
+
+        return cannonBall;
+    }
+
+    [SerializeField] private float speed;
+    [SerializeField] private float despawnTimer;
+
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate() {
+        despawnTimer -= Time.deltaTime;
+        if (despawnTimer <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,53 +1,73 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour {
-    // private float speed = 0;
 
-    //this is our target velocity while decelerating
-    [SerializeField] private float initialVelocity = 0.0f;
+    public float acceleration;
+    public float steering;
+    private Rigidbody2D rb;
 
-    //this is our target velocity while accelerating
-    [SerializeField] private float finalVelocity = 500.0f;
+    public float maximumSpeed;
 
-    //this is our current velocity
-    [SerializeField] private float currentVelocity = 0.0f;
-
-    //this is the velocity we add each second while accelerating
-    [SerializeField] private float accelerationRate = 10.0f;
-
-    //this is the velocity we subtract each second while decelerating
-    [SerializeField] private float decelerationRate = 50.0f;
-
-    // Update is called once per frame
-    private void Update() {
-        /*
-        if (Input.GetKeyDown(KeyCode.Alpha0)) {
-            speed = 0;
-        } else if (Input.GetKeyDown(KeyCode.Alpha1)) {
-            speed = 1;
-        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
-            speed = 2;
-        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
-            speed = 3;
-        }
-        */
-
-        Debug.Log("test1");
-
-        if (Input.GetKey(KeyCode.W)) {
-            Debug.Log("test2");
-            //add to the current velocity according while accelerating
-            currentVelocity = currentVelocity + (accelerationRate * Time.deltaTime);
-        } else {
-            //subtract from the current velocity while decelerating
-            currentVelocity = currentVelocity - (decelerationRate * Time.deltaTime);
-        }
-
-        //ensure the velocity never goes out of the initial/final boundaries
-        currentVelocity = Mathf.Clamp(currentVelocity, initialVelocity, finalVelocity);
-
-        //propel the object forward
-        transform.Translate(0, currentVelocity, 0);
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
- 
+
+    private void Update() {
+        // Cannon Firing
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            CannonBall.CreateCannonBall(transform.position, -transform.right, rb.velocity);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            CannonBall.CreateCannonBall(transform.position, transform.right, rb.velocity);
+        }
+    }
+
+    private void FixedUpdate() {
+        float h = -Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 speed = transform.up * (v * acceleration);
+
+        rb.AddForce(speed);
+
+        float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+        if (direction >= 0.0f) {
+            rb.rotation += h * steering * (rb.velocity.magnitude / 5.0f);
+            //rb.AddTorque((h * steering) * (rb.velocity.magnitude / 10.0f));
+        } else {
+            rb.rotation -= h * steering * (rb.velocity.magnitude / 5.0f);
+            //rb.AddTorque((-h * steering) * (rb.velocity.magnitude / 10.0f));
+        }
+
+        Vector2 forward = new Vector2(0.0f, 0.5f);
+        float steeringRightAngle;
+        if (rb.angularVelocity > 0) {
+            steeringRightAngle = -90;
+        } else {
+            steeringRightAngle = 90;
+        }
+
+        Vector2 rightAngleFromForward = Quaternion.AngleAxis(steeringRightAngle, Vector3.forward) * forward;
+        // Debug.DrawLine((Vector3)rb.position, (Vector3)rb.GetRelativePoint(rightAngleFromForward), Color.green);
+
+        float driftForce = Vector2.Dot(rb.velocity, rb.GetRelativeVector(rightAngleFromForward.normalized));
+
+        Vector2 relativeForce = (rightAngleFromForward.normalized * -1.0f) * (driftForce * 10.0f);
+
+        // Debug.DrawLine((Vector3)rb.position, (Vector3)rb.GetRelativePoint(relativeForce), Color.red);
+
+        rb.AddForce(rb.GetRelativeVector(relativeForce));
+
+        float currentSpeed = Vector3.Magnitude(rb.velocity);  // test current object speed
+
+        if (currentSpeed > maximumSpeed) {
+            float brakeSpeed = currentSpeed - maximumSpeed;  // calculate the speed decrease
+
+            Vector3 normalisedVelocity = rb.velocity.normalized;
+            Vector3 brakeVelocity = normalisedVelocity * brakeSpeed;  // make the brake Vector3 value
+
+            rb.AddForce(-brakeVelocity);  // apply opposing brake force
+        }
+    }
 }
